7316ade9bc8a6582b2d023ce6ce610a1f56db2dd
xcolon <ch.chaweechan@gmail.com>
2020-01-09 19:07:41 +0700

    First Commit

---
+++ src/js/general.js
@@ -0,0 +1,7 @@
+import "../../node_modules/bootstrap-jquery/less/bootstrap.less";
+import "../../node_modules/bootstrap/dist/js/bootstrap.js";
+import '../css/styles.css';
+
+import 'bootstrap';
+
+console.log('General JS File');
